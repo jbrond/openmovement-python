ba5f56e074f3b66ec57b66dc11e5400aa6004855
Jan Christian Brønd <jcb@video4coach.com>
2025-02-26 12:26:53 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='openmovement-python',
+    version='0.0.2',
+    packages=['src', 'src.openmovement', 'src.openmovement.load', 'src.openmovement.process',
+              'src.openmovement.experimental'],
+    url='',
+    license='',
+    author='jb',
+    author_email='jcb@video4coach.com',
+    description=''
+)
